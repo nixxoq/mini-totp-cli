d0e79eabf3d03d5cf97d55eb71a60d6ed75030b2
nixxoq <nixxoq@outlook.com>
2025-06-14 18:34:48 +0300

    core: ui changes (missing utils)

---
+++ src/utils.rs
@@ -0,0 +1,21 @@
+use crossterm::terminal::{Clear, ClearType};
+
+use crate::totp::Totp;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub fn print_accounts(accounts: &[Totp]) -> Result<(), Box<dyn std::error::Error>> {
+    let time_left = 30 - (SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() % 30);
+
+    for account in accounts {
+        let code = account.generate(None)?;
+        println!(
+            "{:<10}: {}  (update in {:02} sec) {}",
+            account.name,
+            code,
+            time_left,
+            Clear(ClearType::UntilNewLine)
+        );
+    }
+
+    Ok(())
+}
